6cd84662241fc80b6c183d346b5e65845b301e3c
pierce <zzpierce@163.com>
2018-06-11 09:02:17 +0800

    BRTree implement

--- src/main/java/com/zz/lib/BRTree.java
+++ src/main/java/com/zz/lib/BRTree.java
@@ -5,6 +5,33 @@ package com.zz.lib;
  */
 public class BRTree<K, V> {
 
+    TreeNode root;
 
+    BRTree() {
+        root = null;
+    }
+
+    public void put(K key, V value) {
+        if (root == null) {
+            root = new TreeNode(key, value, false);
+        }
+    }
+
+
+    class TreeNode {
+
+        TreeNode(K key, V value, boolean red) {
+            this.key = key;
+            this.value = value;
+            this.red = red;
+        }
+
+        K key;
+        V value;
+        boolean red;
+
+        TreeNode left;
+        TreeNode right;
+    }
 
 }
