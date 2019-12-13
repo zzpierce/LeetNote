a524db32fa60355ac3747700c68c800d31105d28
pierce <zzpierce@163.com>
2019-12-14 00:24:20 +0800

    add xmind

---
+++ src/main/java/com/zz/leetcode/p887_super_egg_drop/Problem887.java
@@ -0,0 +1,18 @@
+package com.zz.leetcode.p887_super_egg_drop;
+
+/**
+ * You are given K eggs, and you have access to a building with N floors from 1 to N.
+ * Each egg is identical in function, and if an egg breaks, you cannot drop it again.
+ * You know that there exists a floor F with 0 <= F <= N such that any egg dropped at a floor higher than F will break, and any egg dropped at or below floor F will not break.
+ * Each move, you may take an egg (if you have an unbroken one) and drop it from any floor X (with 1 <= X <= N).
+ * Your goal is to know with certainty what the value of F is.
+ * What is the minimum number of moves that you need to know with certainty what F is, regardless of the initial value of F?
+ */
+public class Problem887 {
+}
+
+class Solution {
+    public int superEggDrop(int K, int N) {
+        //todo
+    }
+}
